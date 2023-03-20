052354807656a4182b0a69bcbf82b55e08e93a55
willmccarten <william.mccarten@ufl.edu>
2023-03-19 23:52:50 -0400

    cleanup of useless files and simple setups

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import catlogo from './assets/catlogo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={catlogo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Cat Zoo!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/willmccarten/catzoo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
