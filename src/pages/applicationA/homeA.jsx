116051e6c703d0568a8d3b4f946843d81a919601
willmccarten <william.mccarten@ufl.edu>
2023-03-20 01:23:52 -0400

    basic styling to pages

--- src/pages/applicationA/homeA.jsx
+++ src/pages/applicationA/homeA.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 const homeA = () => {
     return (
-        <div>
-            <h1>This is Home A</h1>
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Welcome to Cat Zoo Application A!
+                </p>
+
+                <div>
+                
+                
+                </div>
+
+                <a className="App-link" href="https://github.com/willmccarten/catzoo" target="_blank" rel="noopener noreferrer">
+                    Github
+                </a>
+            
+            </header>
         </div>
     );
 }
--- src/pages/applicationB/homeB.jsx
+++ src/pages/applicationB/homeB.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 const homeB = () => {
     return (
-        <div>
-            <h1>This is Home B</h1>
+        <div className="App">
+            <header className="App-header">
+
+                <p>
+                    Welcome to Cat Zoo Application B!
+                </p>
+
+                <div>
+                
+                
+                </div>
+
+                <a className="App-link" href="https://github.com/willmccarten/catzoo" target="_blank" rel="noopener noreferrer">
+                    Github
+                </a>
+            
+            </header>
         </div>
     );
 }
