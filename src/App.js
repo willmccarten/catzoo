fda3d4d28852e3916dce90fea0097e984161185d
willmccarten <william.mccarten@ufl.edu>
2023-03-20 00:07:30 -0400

    adjust app.js html

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={catlogo} className="App-logo" alt="logo" />
+
         <p>
           Welcome to Cat Zoo!
         </p>
-        <a
-          className="App-link"
-          href="https://github.com/willmccarten/catzoo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        <a className="App-link" href="https://github.com/willmccarten/catzoo" target="_blank" rel="noopener noreferrer">
           Github
         </a>
+        
       </header>
     </div>
   );
